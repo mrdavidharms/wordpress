c40b57d577c3038a8fe1b5f3120d033e1a9cd504
David Harms <davidharms@Davids-MacBook-Pro-2.local>
2016-06-06 23:28:09 -0400

    final

--- wp-content/themes/twentythirteen-child/functions.php
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -1,11 +1,22 @@
-<?php 
+<?php
+
+// Create a custom menus
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'footer-menu' => __( 'Footer Menu' )
+    
+    )
+  );
+}
+add_action( 'init', 'register_my_menus' );
 
 
 // Function for creating Widegets
 function create_widget($name, $id, $description) {
- 
+
     register_sidebar(array(
-        'name' => __( $name ),    
+        'name' => __( $name ),
         'id' => $id,
         'description' => __( $description ),
         'before_widget' => '<div id="'.$id.'" class="widget %1$s %2$s">',
@@ -13,10 +24,11 @@ function create_widget($name, $id, $description) {
         'before_title' => '<h3>',
         'after_title' => '</h3>'
     ));
- 
+
 }
- 
+
 // Create the actual widgets
 create_widget("Header", "uptop", "Displays in the header of the site, above the title");
+
 
 ?>
